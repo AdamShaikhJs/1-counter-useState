4ecb6fa2c5752a045cfc163f9abd1d4e28285dd9
Adam Shaikh <adam.shaikh@niveussolutions.com>
2023-01-16 21:41:52 +0530

    changes done counter

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
 
 function App() {
+const [count,setCount]=useState(0)
+const complexIncress=()=>{
+  setTimeout(()=>{
+    setCount((pre)=>pre+1)
+  },2000)
+}
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-          DDDon ghhghghghg
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React 
-        </a>
+        <h1> count : {count}</h1>
+         <div>
+        <button onClick={()=>setCount(count+1)}>increment </button> 
+        <button onClick={()=>setCount(0)}>reset </button> 
+        <button onClick={()=>setCount(count-1)}> decrement </button> 
+        <button onClick= {()=>setCount((pre)=>pre+count)}>incrementMax </button> 
+        <button onClick= {complexIncress}>complexIncress </button> 
+
+        </div>
       </header>
     </div>
   );
